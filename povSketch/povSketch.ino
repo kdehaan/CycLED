e864b8164da0ef994e1f4f76d974ddea2c00c3c1
Kevin de Haan <kbehaan@gmail.com>
2017-11-18 12:20:49 -0700

    adafruit libraries update to microseconds

--- povSketch/povSketch.ino
+++ povSketch/povSketch.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+  colorWipe(strip.Color(0, 0, 0), 0);
+  Serial.begin(9600);
+  Serial.println("serial port initialized");
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //colorWipe(strip.Color(0, 0, 0), 0);
+  rainbowCycle(2000000);
+}
 
+
+void rainbowCycle(uint32_t waitMicro) {
+  uint16_t i, j;
+  for(j=0; j<256; j++) { // 1 cycle of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delayMicroseconds(waitMicro);
+  }
 }

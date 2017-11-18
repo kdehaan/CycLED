bb67ba607f67f287422496cef1c00b442450e348
Kevin de Haan <kbehaan@gmail.com>
2017-11-18 13:35:31 -0700

    thank god it actually worked

--- povSketch/povSketch.ino
+++ povSketch/povSketch.ino
@@ -3,8 +3,23 @@
   #include <avr/power.h>
 #endif
 
+#define PIN 9
+#define HALL 3
+#define ledPin 13
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -13,6 +28,7 @@ void setup() {
   colorWipe(strip.Color(0, 0, 0), 0);
   Serial.begin(9600);
   Serial.println("serial port initialized");
+  attachInterrupt(digitalPinToInterrupt(HALL), magnet_detect, FALLING);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
@@ -20,9 +36,23 @@ void setup() {
 void loop() {
   //colorWipe(strip.Color(0, 0, 0), 0);
   rainbowCycle(2000000);
+
 }
 
 
+void magnet_detect() {
+  digitalWrite(ledPin, HIGH);
+}
+
+
+// Fill the dots one actual the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delayMicroseconds(wait);
+  }
+}
 void rainbowCycle(uint32_t waitMicro) {
   uint16_t i, j;
   for(j=0; j<256; j++) { // 1 cycle of all colors on wheel
@@ -32,4 +62,17 @@ void rainbowCycle(uint32_t waitMicro) {
     strip.show();
     delayMicroseconds(waitMicro);
   }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }

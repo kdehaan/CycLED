722d1ce16f384fa33b23ee8378be628f7c95ca6c
Kevin de Haan <kbehaan@gmail.com>
2017-11-18 16:00:42 -0700

    testing to find system response

--- povSketch/povSketch.ino
+++ povSketch/povSketch.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include "TimerOne.h"
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -6,16 +7,19 @@
 #define PIN 9
 #define HALL 3
 #define ledPin 13
+#define numLeds 30
+
+unsigned long now = 0;
+unsigned long lastTime = 0;
+unsigned long currentTime = 0;
+unsigned long goal = 9000000;
+unsigned long microDif = goal;
+int lambda = 90;
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numLeds, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -28,21 +32,37 @@ void setup() {
   colorWipe(strip.Color(0, 0, 0), 0);
   Serial.begin(9600);
   Serial.println("serial port initialized");
-  attachInterrupt(digitalPinToInterrupt(HALL), magnet_detect, FALLING);
+  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  //attachInterrupt(digitalPinToInterrupt(HALL), magnet_detect, FALLING);
+  Timer1.initialize(1000000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+  Timer1.stop();
+  Timer1.restart();
+  //Timer1.attachInterrupt(timeout);
 }
 
 void loop() {
-  colorWipe(strip.Color(0, 0, 0), 0);
-  //rainbowCycle(2000000);
+//  rainbowCycle(goal);
+//  microDif = goal;
+//  now = micros();
+//  Serial.print("STARTING LOOP with goal ");
+//  Serial.println(goal/1000);
 
 }
 
 
 void magnet_detect() {
-  digitalWrite(ledPin, HIGH);
+  
+  
 }
+
+//void timeout() {
+//  unsigned long actual = micros()-now;
+//  Serial.print("took ");
+//  Serial.println(actual/1000);
+//  magnet_detect();
+//}
 
 
 // Fill the dots one actual the other with a color
@@ -53,15 +73,26 @@ void colorWipe(uint32_t c, uint8_t wait) {
     delayMicroseconds(wait);
   }
 }
-void rainbowCycle(uint32_t waitMicro) {
+void rainbowCycle(uint32_t goalMicro) {
+  Timer1.restart();
   uint16_t i, j;
   for(j=0; j<256; j++) { // 1 cycle of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      
     }
     strip.show();
-    delayMicroseconds(waitMicro);
+    if(Timer1.read() > (goalMicro - 1000)){
+        Serial.print("got to: ");
+        Serial.println(j);
+        Serial.print("took: ");
+        Serial.println(Timer1.read());
+        return;
+    }
+    delayMicroseconds(3400);
   }
+  Serial.print("took: ");
+  Serial.println(Timer1.read()/1000);
 }
 
 uint32_t Wheel(byte WheelPos) {

ca9aa18332ffd96973858ed12b92fc719774d67b
Kevin de Haan <kbehaan@gmail.com>
2017-11-19 03:18:47 -0700

    adjustment attempt

--- povSketch/povSketch.ino
+++ povSketch/povSketch.ino
@@ -16,7 +16,7 @@ volatile unsigned long now = 0;
 volatile unsigned long lastTime = 0;
 volatile unsigned long currentTime = 0;
 volatile unsigned long goal = 0;
-float slope = 3.89;//3.89;
+float slope = 3.5;//3.89;
 bool on;
 
 float dt = 0.5;
@@ -36,6 +36,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(numLeds, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(button, INPUT);
+  pinMode(HALL, INPUT);
   PCintPort::attachInterrupt(button, changeState, FALLING);
   PCintPort::attachInterrupt(HALL, magnet_detect, FALLING);
   digitalWrite(button, HIGH);
@@ -48,18 +49,22 @@ void setup() {
   // End of trinket special code
   colorWipe(strip.Color(0, 0, 0), 0);
   Serial.begin(9600);
-  Serial.println("serial port initialized");
+  //Serial.println("serial port initialized");
   
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   
-  Timer1.initialize(1000000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+  Timer1.initialize(10000000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
   Timer1.stop();
   Timer1.restart();
   //Timer1.attachInterrupt(timeout);
 }
 
 void changeState() {
+//  int lambda;
+//  lambda = 0;
+//  goal = 803000;
+//  rainbowCycle(goal, lambda, 0);
   on = !on;
   if (on) {
     goal = 0;
@@ -76,10 +81,20 @@ void loop() {
   }
   //cycle();
 
-    
-
+//  int value = 0; 
+//  for (int i = 0; i<=11; i++){ 
+//    int lambda = 200*i; 
+//    rainbowCycle(1000000000, lambda, 0); 
+////    Serial.print("Lambda: "); 
+////    Serial.println(lambda); 
+//     
+//  } 
+//  colorWipe(strip.Color(0, 0, 0), 0); 
+//  exit(0); 
 
 }
+
+
 
 void timeout() {
   goal = 0;
@@ -87,24 +102,24 @@ void timeout() {
 
 void cycle() {
   loop_tracking++;
-  if (loop_tracking > 5) {
+  if (loop_tracking > 4) {
     goal = 0;
     loop_tracking = 0;
   }
-  Serial.print("STARTING LOOP with goal ");
-  Serial.println(goal/1000);
+//  Serial.print("STARTING LOOP with goal ");
+//  Serial.println(goal/1000);
   int lambda;
 
-  if(goal > 802000) {
-    lambda = ((goal/1000) - 802)*slope;
+  if(goal > 804000) {
+    lambda = ((goal/1000) - 804)*slope;
     lambda = (numLeds/60)*lambda;
     rainbowCycle(goal, lambda, 0);
-  } else if (goal > 401000) {
-    lambda = ((goal/1000) - 401)*slope*2;
+  } else if (goal > 402000) {
+    lambda = ((goal/1000) - 402)*slope*2;
     lambda = (numLeds/60)*lambda;
     rainbowCycle(goal, lambda, 1);
-  } else if (goal > 200000) {
-    lambda = ((goal/1000) - 200)*slope*4;
+  } else if (goal > 201000) {
+    lambda = ((goal/1000) - 201)*slope*4;
     lambda = (numLeds/60)*lambda;
     rainbowCycle(goal, lambda, 3);
   } else if (goal > 100000) {
@@ -204,10 +219,14 @@ void rainbowCycle(uint32_t goalMicro, int lambda, int multiplier) {
     }
     strip.show();
     if(Timer1.read() > (goalMicro - 1000)){
+        digitalWrite(ledPin, HIGH);
+      delay(1);
+      digitalWrite(ledPin, LOW);
 //        Serial.print("got to: ");
 //        Serial.println(j);
         //Serial.print("took: ");
         //Serial.println(Timer1.read()/1000);
+        
         return;
     }
     delayMicroseconds(lambda);
@@ -216,6 +235,9 @@ void rainbowCycle(uint32_t goalMicro, int lambda, int multiplier) {
   
  // Serial.print("took: ");
  // Serial.println(Timer1.read()/1000);
+//  char buf[10]
+//  sprintf(buf, "GET /testID=%lu HTTP/1.0", testID); 
+//  
 }
 
 
